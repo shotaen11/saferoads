4dbf4235b4968ce63cd0d4285bd6656061f3165d
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-11-17 19:56:23 +0900

    devise機能

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_16_120950) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_17_104440) do
   create_table "road_conditions", force: :cascade do |t|
     t.string "road_name"
     t.string "road_status"
@@ -20,6 +20,19 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_16_120950) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "image_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
