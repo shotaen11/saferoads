d3ba9155455e052567c8d6a83fd5d5f9f3b32a5d
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2024-12-01 15:30:27 +0900

    タイトルレイアウト調整

--- app/models/road_condition.rb
+++ app/models/road_condition.rb
@@ -7,8 +7,7 @@ class RoadCondition < ApplicationRecord
 
     validates :road_name, presence: true
     validates :road_status, presence: true
-    validates :description, presence: true, length: { maximum: 195 }
-    validates :image, presence: true  
+    validates :description, presence: true, length: { maximum: 195 } 
 
     # enum カラム名：{名前:番号}
     enum status: { published: 0, draft: 1 }
