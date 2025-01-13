90d832b4219e15b010f2c996c421660f25d9580a
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2025-01-13 13:17:47 +0900

    ハンバーガーメニュー

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,10 +1,1 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-// application.js
-import "jquery"  // importmapからjQueryを読み込み
-import "@rails/ujs";
-import "bootstrap"
-import "@hotwired/turbo-rails"
-import "controllers"
-// app/javascript/application.js
-import '../hamburger_menu';
-
