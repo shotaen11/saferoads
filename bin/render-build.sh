d2b7b3db1546d926e088b77fbdcd7752b921e40a
shotaen11 <180824926+shotaen11@users.noreply.github.com>
2025-03-02 16:49:37 +0900

    re6

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -8,3 +8,5 @@ bundle exec rails assets:precompile
 # 以下追加
 bundle exec rake assets:clean
 bundle exec rake db:migrate
+# bundle exec rake db:migrate
+DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
